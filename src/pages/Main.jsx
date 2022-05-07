a947185e4e2e58ebe89d9666ed7a64e9c7a0ff62
MuhammadaminAbbosov <musulmaanjanob@gmail.com>
2022-05-07 13:11:39 +0500

    calculator build

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -3,6 +3,47 @@ import styled from "styled-components";
 
 export default function Main({setChangeColor}) {
   const [changeTheme, setChangeTheme] = useState(1)
+
+  const [calc, setCalc]= useState("")
+  const [result , setResult] = useState("")
+
+  const ops = ["/", "*", "+", "-", "."]
+
+
+  const updateCalc = value => {
+    if (
+      (ops.includes(value) && calc === "" )|| 
+      (ops.includes(value) && ops.includes(calc.slice(-1)))
+    ) {
+      return; 
+    }
+
+    setCalc(calc + value)
+
+    if(!ops.includes(value)) {
+      setResult(eval(calc + value).toString( ))
+    }
+  }
+
+  const calculate = () => {
+    setCalc(eval(calc).toString())
+  }
+
+  const deleteLast = () => {
+    if(calc === "" ) return;
+
+    const value = calc.slice(0, -1)
+
+    setCalc(value)
+
+    setResult(eval(value).toString())
+  }
+
+  const reset = () => {
+    setCalc("")
+    setResult("")
+  }
+
   return (
     <Wrapper changeTheme={changeTheme}>
       <header>
@@ -25,28 +66,29 @@ export default function Main({setChangeColor}) {
       </header>
 
       <div className="screen">
-        <b>0</b>
+        {result ? <p>({result})</p> : ""}
+        <b>{calc || "0"}</b>
       </div>
 
       <div className="buttons-container">
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>DEL</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>+</button>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>-</button>
-        <button>.</button>
-        <button>0</button>
-        <button>/</button>
-        <button>x</button>
-        <button>RESET</button>
-        <button>=</button>
+        <button onClick={() => updateCalc("7")}>7</button>
+        <button onClick={() => updateCalc("8")}>8</button>
+        <button onClick={() => updateCalc("9")}>9</button>
+        <button onClick={() => deleteLast()}>DEL</button>
+        <button onClick={() => updateCalc("4")}>4</button>
+        <button onClick={() => updateCalc("5")}>5</button>
+        <button onClick={() => updateCalc("6")}>6</button>
+        <button onClick={() => updateCalc("+")}>+</button>
+        <button onClick={() => updateCalc("1")}>1</button>
+        <button onClick={() => updateCalc("2")}>2</button>
+        <button onClick={() => updateCalc("3")}>3</button>
+        <button onClick={() => updateCalc("-")}>-</button>
+        <button onClick={() => updateCalc(".")}>.</button>
+        <button onClick={() => updateCalc("0")}>0</button>
+        <button onClick={() => updateCalc("/")}>/</button>
+        <button onClick={() => updateCalc("*")}>x</button>
+        <button onClick={() => reset()}>RESET</button>
+        <button onClick={calculate}>=</button>
       </div>
     </Wrapper>
   );
@@ -192,8 +234,16 @@ const Wrapper = styled.div`
       }
     }};
     border-radius: 16px;
-    font-size: 36px;
-    text-align: right;
+    font-size: 34px;
+
+    display: flex;
+    align-items: flex-end;
+    justify-content: flex-end;
+    gap: 10px;
+
+    p {
+      font-size: 20px;
+    }
     color: ${({ changeTheme }) => {
       if (changeTheme === 1) {
         return "white";
